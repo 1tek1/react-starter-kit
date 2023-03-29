f22272d9a9a9947f4cb58c4c38b1efa8460982a4
1tek1 <109482319+1tek1@users.noreply.github.com>
2023-03-30 00:25:29 +0300

    ilk react proje kodları

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     test12
+     <img src="/logo192.png"/>
+    </div>
+  );
+}
+
+export default App;
